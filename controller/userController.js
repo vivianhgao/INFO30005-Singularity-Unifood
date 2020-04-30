dcc1a9dcc04ec36b28cd96774915f08babac63d4
pbudiman <61896774+pbudiman@users.noreply.github.com>
2020-04-30 18:33:43 +1000

    added more views

--- controller/userController.js
+++ controller/userController.js
@@ -3,37 +3,26 @@ const mongoose = require("mongoose");
 // import user model
 const User = mongoose.model("users");
 
-// function to handle a request to get all users
-const getAllUsers = async (req, res) => {
+// function for user to log in
+const logIn = (req, res, next) => {
+    var username =  req.body.username;
+    var password =  req.body.password;
 
-    try {
-        const all_user = await User.find();
-        return res.send(all_user);
-    } catch (err) {
-        res.status(400);
-        return res.send("Database query failed!");
-    }
-};
-
-//function to update a user
-const updateUser =  async (req, res) =>{
-    var condition = {username: req.params.username};
-
-    User.findOneAndUpdate(condition, { $set: req.body }, function(err,user){
-        if (err){
-            console.error('An error occured!');
+    //find the user using the log in user name
+    User.findOne({username:username},function (err,user){
+        if (err) {
+            console.error("An error occured.");
         }
-        else if(!user){
-            return res.send('User is not found!');
+        else if(!user || password!=user.password){
+            res.send("Wrong username/password.")
         }
         else {
-            return res.send("User is updated!");
+            res.render('welcomeUser',{ first_name:user.first_name, username:username });
         }
     });
 };
 
-
-// function to add user account
+// function to add user account when a new user sign up
 const addUser = async (req, res) => {
     var new_user = {
         username: req.body.username,
@@ -42,6 +31,7 @@ const addUser = async (req, res) => {
         first_name: req.body.first_name,
         last_name: req.body.last_name
     }
+    // check if the username/email has been registered
     User.exists({username:req.body.username} || {email:req.body.email},function (err,userExists) {
         if(err){
             res.send('An error occured');
@@ -51,17 +41,15 @@ const addUser = async (req, res) => {
         }
         else{
             var data =  new User(new_user);
-
             data.save();
-
-            res.send('New user added!');
+            res.render('welcomeUser',{first_name:req.body.first_name});
         }
     });
 };
 
-// function to get user by username
-const getUserByUsername = (req, res,next) => {
-    var requested =  req.body.username;
+//get the details of the user
+const getDetails = (req,res,next) => {
+    var requested=  req.params.username;
 
     User.findOne({username:requested},function (err,user){
         if (err) {
@@ -71,12 +59,47 @@ const getUserByUsername = (req, res,next) => {
             res.send("No user with that username.")
         }
         else {
-            res.send(user);
+            res.render('userDetails',{first_name:user.first_name, last_name:user.last_name,username:user.username});
         }
     });
 };
 
+<<<<<<< Updated upstream
 // function to delete a user
+=======
+//function to update an information about a user
+const updateUser =  async (req, res) => {
+    var condition = {username: req.params.username};
+    var update = {
+        username: req.body.username,
+        email: req.body.email,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        password: req.body.password
+    }
+    //clean updated field
+    for( field in update ){
+        if(update[field] ==''){
+            delete update[field]
+        }
+    }
+
+    //find the user's details and update it
+    User.findOneAndUpdate(condition, update, function(err,user){
+        if (err){
+            console.error('An error occured!');
+        }
+        else if(!user){
+            return res.send('User is not found!');
+        }
+        else {
+            res.send("User is updated!");
+        }
+    });
+};
+
+// function to delete a user when the user wants to delete their account
+>>>>>>> Stashed changes
 const deleteUser = (req,res) => {
     var requested = req.params.username;
 
@@ -88,14 +111,29 @@ const deleteUser = (req,res) => {
             res.send("User '" + requested + "' is successfully deleted!");
         }
     });
+};
 
+// function to get all users
+const getAllUsers = async (req, res) => {
+    try {
+        const all_user = await User.find();
+        return res.send(all_user);
+    } catch (err) {
+        res.status(400);
+        return res.send("Database query failed!");
+    }
 };
 
 // export the functions
 module.exports = {
     getAllUsers,
+<<<<<<< Updated upstream
     getUserByUsername,
+=======
+    logIn,
+>>>>>>> Stashed changes
     addUser,
+    getDetails,
     updateUser,
     deleteUser
 };
