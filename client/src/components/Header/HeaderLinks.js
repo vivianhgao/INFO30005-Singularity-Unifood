b94cd0dca7eaa025d302f85d15374344095b269f
vivianhgao <62043055+vivianhgao@users.noreply.github.com>
2020-05-13 21:18:01 +1000

    updated front page

---
+++ client/src/components/Header/HeaderLinks.js
@@ -0,0 +1,77 @@
+/*eslint-disable*/
+import React from "react";
+import DeleteIcon from "@material-ui/icons/Delete";
+import IconButton from "@material-ui/core/IconButton";
+// react components for routing our app without refresh
+import { Link } from "react-router-dom";
+
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import Tooltip from "@material-ui/core/Tooltip";
+
+// @material-ui/icons
+import { Apps, CloudDownload } from "@material-ui/icons";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+
+// core components
+import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
+import Button from "components/CustomButtons/Button.js";
+
+import styles from "assets/jss/material-kit-react/components/headerLinksStyle.js";
+
+const useStyles = makeStyles(styles);
+
+export default function HeaderLinks(props) {
+  const classes = useStyles();
+  return (
+    <List className={classes.list}>
+      <ListItem className={classes.listItem}>
+        <CustomDropdown
+          noLiPadding
+          buttonText="Navigation"
+          buttonProps={{
+            className: classes.navLink,
+            color: "danger"
+          }}
+          buttonIcon={Apps}
+          dropdownList={[
+            <Link to="/" className={classes.dropdownLink}>
+              All components
+            </Link>,
+            <a
+              href="https://creativetimofficial.github.io/material-kit-react/#/documentation?ref=mkr-navbar"
+              target="_blank"
+              className={classes.dropdownLink}
+            >
+              Documentation
+            </a>
+          ]}
+        />
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="https://www.creative-tim.com/product/material-kit-react?ref=mkr-navbar"
+          color="danger"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <ExitToAppIcon className={classes.icons} /> Sign Up
+        </Button>
+      </ListItem>
+
+        <ListItem className={classes.listItem}>
+            <Button
+                href="https://www.creative-tim.com/product/material-kit-react?ref=mkr-navbar"
+                color="danger"
+                target="_blank"
+                className={classes.navLink}
+            >
+                <ExitToAppIcon className={classes.icons} /> Log In
+            </Button>
+        </ListItem>
+    </List>
+  );
+}
